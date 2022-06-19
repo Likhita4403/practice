40d0ccc954ea5c9be2fd3876032726d98e225e4f
Sai Preeti <psaipreeti@gmail.com>
2022-06-19 13:22:35 +0530

    Navbar done

--- gitpractice/src/App.js
+++ gitpractice/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavbarComp from './components/Navbar/NavbarComp';
+import Navbar from './components/Navbar/Navbar';
 // import Home from './components/Home';
 // import About from './components/About';
 // import ContactUs from './Components/ContactUs';
 // import Login from './Components/Login';
+import Drawer from './components/Navbar/DrawerCom';
 function App() {
   return (
     <div className="App">
-     <NavbarComp/>
+     <Navbar/>
     </div>
   );
 }
